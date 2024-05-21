70011fa3b5ee541f27d9df527c6968ef9829f403
Minh <thietbicongnghevietnam@gmail.com>
2024-05-21 19:00:28 +0700

    21052024b

---
+++ WebApplication1/MakeQRcode.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MakeQRcode.aspx.cs" Inherits="WebApplication1.MakeQRcode" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+     <div class="container">  
+            <h2>Auto generate QR Code</h2>  
+            <div class="row">  
+                <div class="col-md-4">  
+                    <div class="form-group">  
+                        <label>Enter Something</label>  
+                        <div class="input-group">  
+                            <asp:TextBox ID="txtQRCode" runat="server" CssClass="form-control"></asp:TextBox>  
+                            <div class="input-group-prepend">  
+                                <asp:Button ID="btnGenerate" runat="server" CssClass="btn btn-secondary" Text="Generate" OnClick="btnGenerate_Click" />  
+                            </div>  
+                        </div> 
+                        <label>Width</label>  
+                        <div class="input-group">  
+                            <asp:TextBox ID="txt_width" runat="server" onkeyup="if (/\D/g.test(this.value)) this.value = this.value.replace(/\D/g,'')" CssClass="form-control"></asp:TextBox>                              
+                        </div>  
+                        <label>Height</label>  
+                        <div class="input-group">  
+                            <asp:TextBox ID="txt_height" runat="server" onkeyup="if (/\D/g.test(this.value)) this.value = this.value.replace(/\D/g,'')" CssClass="form-control"></asp:TextBox>                             
+                        </div>  
+                    </div>  
+                </div>  
+            </div>         
+               <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>                        
+        </div>  
+    <script type="text/javascript">       
+        $("#MainContent_txt_width").on('keyup', function (e) {
+            //debugger;
+            if (parseInt((this.value)) > 1000) {
+                alert('Please input witdh under 1000');
+                $("#MainContent_txt_width").val('');
+            }
+        });
+
+        $("#MainContent_txt_height").on('keyup', function (e) {
+            if (parseInt((this.value)) > 1000) {
+                alert('Please input height under 1000');
+                $("#MainContent_txt_height").val('');
+            }
+        });
+    </script>
+
+</asp:Content>
---
+++ WebApplication1/MakeQRcode.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+
+
+namespace WebApplication1
+{
+    public partial class MakeQRcode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //protected void btnGenerate_Click(object sender, EventArgs e)
+        //{
+        //    if (txtQRCode.Text != "" && txt_width.Text != "" && txt_height.Text != "")
+        //    {
+        //        string code = txtQRCode.Text;
+        //        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        //        QRCodeData qrdata = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+        //        QRCode qrCode = new QRCode(qrdata);
+        //        System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+        //        imgBarCode.Height = int.Parse(txt_height.Text);
+        //        imgBarCode.Width = int.Parse(txt_width.Text);
+        //        using (Bitmap bitMap = qrCode.GetGraphic(20))
+        //        {
+        //            using (MemoryStream ms = new MemoryStream())
+        //            {
+        //                bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+        //                byte[] byteImage = ms.ToArray();
+        //                imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+        //            }
+        //            PlaceHolder1.Controls.Add(imgBarCode);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Page.ClientScript.RegisterStartupScript(Page.GetType(), "Message", "alert('ERROR, Please input enough information again!'); ", true);
+        //    }
+
+        //}
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Tạo mã QR
+            string qrContent = txtQRCode.Text; // Lấy nội dung từ TextBox
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrContent, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            // Chuyển đổi mã QR thành hình ảnh
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            // Lưu hình ảnh vào bộ nhớ đệm và gán đường dẫn cho control hình ảnh
+            MemoryStream ms = new MemoryStream();
+            qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            byte[] byteImage = ms.ToArray();
+            qrCodeImage.Dispose();
+            //qrCodeImage.Suspend();
+            string base64String = Convert.ToBase64String(byteImage, 0, byteImage.Length);
+            string imageUrl = "data:image/png;base64," + base64String;
+
+            // Hiển thị hình ảnh QR trên giao diện
+            //PlaceHolder1.Controls.Add(new Image { ImageUrl = imageUrl });
+        }
+
+
+    }
+}
